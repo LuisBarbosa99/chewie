ac959c584581f86c4df5f74f1ce2fe7edc9ee03f
Luis Barbosa <luis.barbosa@upe.br>
2020-10-26 17:48:16 -0300

    updated database.js config

--- config/database.js
+++ config/database.js
@@ -23,7 +23,7 @@ module.exports = {
   mysql: {
     client: "mysql",
     connection: {
-      host: Env.get("DB_HOST", 'localhost'),
+      host: Env.get("DB_HOST", '52.86.255.97'),
       port: Env.get("DB_PORT", ""),
       user: Env.get("DB_USER", 'root'),
       password: Env.get("DB_PASSWORD", ''),
